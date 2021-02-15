41dd62d0691ca334d378e39c44c03c1be5bdfd4e
EverChicas <2373708320101@ingenieria.usac.edu.gt>
2021-02-15 13:25:58 -0600

    funcional

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-"use strict"
 
 var canvas = document.getElementById('game');
 var ctx = canvas.getContext('2d');
+var pathResources = '../resources';
+var imgs = [`${pathResources}/enemyShispace.png`,`${pathResources}/laser.png`,`${pathResources}/laser2.png`,`${pathResources}/shispace.png`,`${pathResources}/shispace2.png`,`${pathResources}/space2.jpeg`];
+var preloader;
 var shispace = {x:canvas.width/2,y:canvas.height-75,width:50,height:50,state:'alive',counter:0}
 var game = {state:'init'};
 var replyText = {counter:-1,title:'',subtitle:''};
@@ -9,15 +11,48 @@ var keyboard = {};
 var shots = [];
 var enemyShots = [];
 var enemies = [];
-var fondo;
+var fondo, imgShispace, imgEnemy, imgLaser, imgLaserEnemy;
+var soundShot, soundShot2, soundDeadShispace, soundDeadEnemy;
 
 
 function loadMedia(){
-    fondo = new Image();
-    fondo.src = '../resources/space2.jpeg';
-    fondo.onload = function(){
-        var intervalo = window.setInterval(frameLoop,1000/55);
+    preloader = new createjs.LoadQueue();
+    preloader.onProgress = progressLoader();
+    loader();
+}
+
+function loader(){
+    while(imgs.length > 0){
+        let img = imgs.shift();
+        preloader.loadFile(img);
     }
+}
+
+function progressLoader(){
+    console.log(parseInt(preloader.progress * 100)+"%");
+    var intervalo = window.setInterval(frameLoop,1000/55);
+    fondo = new Image();
+    fondo.src = imgs[5];
+    imgShispace = new Image();
+    imgShispace.src = imgs[3];
+    imgEnemy = new Image();
+    imgEnemy.src = imgs[0];
+    imgLaser = new Image();
+    imgLaser.src = imgs[1];
+    imgLaserEnemy = new Image();
+    imgLaserEnemy.src = imgs[2];
+    soundShot = document.createElement('audio');
+    document.body.appendChild(soundShot);
+    soundShot.setAttribute('src',`${pathResources}/SoundLacer.mp3`);
+    soundShot2 = document.createElement('audio');
+    document.body.appendChild(soundShot2);
+    soundShot2.setAttribute('src',`${pathResources}/SoundLacer2.mp3`);
+    soundDeadShispace = document.createElement('audio');
+    document.body.appendChild(soundDeadShispace);
+    soundDeadShispace.setAttribute('src',`${pathResources}/BombShispace.mp3`);
+    soundDeadEnemy = document.createElement('audio');
+    document.body.appendChild(soundDeadEnemy);
+    soundDeadEnemy.setAttribute('src',`${pathResources}/BombEnemy.mp3`);
 }
 
 function drawEnemies(){
@@ -26,10 +61,11 @@ function drawEnemies(){
 
         ctx.save();
 
-        if(enemy.state == 'alive') ctx.fillStyle = 'red';
+        // if(enemy.state == 'alive') ctx.fillStyle = 'red';
         if(enemy.state == 'dead') ctx.fillStyle = 'black';
 
-        ctx.fillRect(enemy.x,enemy.y,enemy.width,enemy.height);
+        // ctx.fillRect(enemy.x,enemy.y,enemy.width,enemy.height);
+        ctx.drawImage(imgEnemy,enemy.x,enemy.y,enemy.width,enemy.height);
     }
 }
 
@@ -39,8 +75,9 @@ function drawBackground(){
 
 function drawShispace(){
     ctx.save();
-    ctx.fillStyle = 'white';
-    ctx.fillRect(shispace.x,shispace.y,shispace.width,shispace.height);
+    // ctx.fillStyle = 'white';
+    // ctx.fillRect(shispace.x,shispace.y,shispace.width,shispace.height);
+    ctx.drawImage(imgShispace,shispace.x,shispace.y,shispace.width,shispace.height);
     ctx.restore();
 }
 
@@ -113,16 +150,20 @@ function moveShooting(){
 }
 
 function fire(){
+    soundShot.pause();
+    soundShot.currentTime = 0;
+    soundShot.play();
     shots.push({x:shispace.x + 20, y:shispace.y - 10,width:10,height:30});
 }
 
 function drawShot(){
     ctx.save();
-    ctx.fillStyle = 'white';
+    // ctx.fillStyle = 'white';
 
     for(let i in shots){
         let shot = shots[i];
-        ctx.fillRect(shot.x,shot.y,shot.width,shot.height);
+        // ctx.fillRect(shot.x,shot.y,shot.width,shot.height);
+        ctx.drawImage(imgLaser,shot.x,shot.y,shot.width,shot.height);
     }
 
     ctx.restore();
@@ -149,6 +190,9 @@ function upgradeEnemies(){
             enemy.x += Math.sin(enemy.counter * Math.PI/90)*5;
 
             if(random(0,enemies.length*10) == 4){
+                soundShot2.pause();
+                soundShot2.currentTime = 0;
+                soundShot2.play();
                 enemyShots.push(addEnemyShot(enemy));
             }
 
@@ -208,6 +252,9 @@ function verifyContact(){
         for(let j in enemies){
             let enemy = enemies[j];
             if(hit(shot,enemy)){
+                soundDeadShispace.pause();
+                soundDeadShispace.currentTime = 0;
+                soundDeadShispace.play();
                 enemy.state = 'hit';
                 enemy.counter = 0;
             }
@@ -219,6 +266,9 @@ function verifyContact(){
     for(let i in enemyShots){
         let shot = enemyShots[i];
         if(hit(shot,shispace)){
+            soundDeadEnemy.pause();
+            soundDeadEnemy.currentTime = 0;
+            soundDeadEnemy.play();
             shispace.state = 'hit';
         }
     }
@@ -227,11 +277,12 @@ function verifyContact(){
 
 function drawEnemyShots(){
     ctx.save();
-    ctx.fillStyle = 'yellow';
+    // ctx.fillStyle = 'yellow';
 
     for(let i in enemyShots){
         let shot = enemyShots[i];
-        ctx.fillRect(shot.x,shot.y,shot.width,shot.height);
+        // ctx.fillRect(shot.x,shot.y,shot.width,shot.height);
+        ctx.drawImage(imgLaserEnemy,shot.x,shot.y,shot.width,shot.height);
     }
     ctx.restore();
 }
@@ -321,6 +372,10 @@ function frameLoop(){
     drawText();
 }
 
+function init(){
+    loadMedia();
+    addKeyboardEvents();
+}
 
-loadMedia();
-addKeyboardEvents();
+window.addEventListener('load',init);
+
